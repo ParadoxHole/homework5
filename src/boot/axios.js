c52f5b16e71bebe2361773d3eed3102216d440d6
ParadoxHole <paradoxholestudio@gmail.com>
2023-11-20 10:54:59 +0700

    'edfs'

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 // const RESTAPI = "http://localhost:3000/api"
-const RESTAPI = "https://manage-account-eight.vercel.app/api"
+const RESTAPI = "https://homework5back-7kg6.vercel.app/api"
 
 const api = axios.create({ 
   baseURL: RESTAPI,
